2e648fb4c3b093b344767725c4c0bd37dd2039c7
Sarvesh Kulkarni <30865213+snk95@users.noreply.github.com>
2020-05-20 22:08:00 -0700

    log in and signup page half done
    log in and signup page half done and firebase set up

--- app/src/main/java/com/example/coen268project/View/MainActivity.java
+++ app/src/main/java/com/example/coen268project/View/MainActivity.java
@@ -1,18 +1,86 @@
 package com.example.coen268project.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 import com.example.coen268project.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    EditText usernameEditText, passwordEditText;
+    Button logInBtn, guestLoginBtn;
+    TextView signUpTextView, forgotPasswordTextView;
+    FirebaseAuth mFirebaseAuth ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        usernameEditText = findViewById(R.id.usernameEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        logInBtn = findViewById(R.id.logInBtn);
+        guestLoginBtn = findViewById(R.id.guestBtn);
+        signUpTextView = findViewById(R.id.signUpTextView);
+        forgotPasswordTextView = findViewById(R.id.forgotPasswordTextView);
+        mFirebaseAuth=FirebaseAuth.getInstance();
+
+        signUpTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,SignUp.class));
+            }
+        });
+
+        logInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email=usernameEditText.getText().toString();
+                String password=passwordEditText.getText().toString();
+                if(email.isEmpty() ){
+                    usernameEditText.setError("Please enter email id");
+                    usernameEditText.requestFocus();
+                }
+                else if(password.isEmpty() ){
+                    passwordEditText.setError("Please enter password");
+                    passwordEditText.requestFocus();
+                }
+
+                else {
+                    //login will take place and user will go to home screen
+                }
+
+            }
+        });
+
+        guestLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,SignUp.class)); // change this to homescreen once activity is created
+            }
+        });
+
+        forgotPasswordTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // handle forgot password
+            }
+        });
+
     }
 
-    //hello world
+
 }
---
+++ app/src/main/java/com/example/coen268project/View/SignUp.java
@@ -0,0 +1,100 @@
+package com.example.coen268project.View;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.coen268project.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignUp extends AppCompatActivity {
+    EditText fullnameEditText, emailEditText, passwordEditText, confirmPasswordEditText;
+    Button signUpBtn;
+    TextView signInTextView;
+    FirebaseAuth mFirebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        fullnameEditText=findViewById(R.id.fullnameEditText);
+        emailEditText=findViewById(R.id.emailEditText);
+        passwordEditText=findViewById(R.id.passwordEditText);
+        confirmPasswordEditText=findViewById(R.id.confirmpasswordEditText);
+        signUpBtn=findViewById(R.id.signUpBtn);
+        signInTextView=findViewById(R.id.signInTextView);
+
+        mFirebaseAuth=FirebaseAuth.getInstance();
+
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fullname=fullnameEditText.getText().toString();
+                String email=emailEditText.getText().toString();
+                String password=passwordEditText.getText().toString();
+                String confirmPassword=confirmPasswordEditText.getText().toString();
+
+                if(email.isEmpty() ){
+                    emailEditText.setError("Please enter email id");
+                    emailEditText.requestFocus();
+                }
+
+                else if(fullname.isEmpty() ){
+                    fullnameEditText.setError("Please enter fullname");
+                    fullnameEditText.requestFocus();
+                }
+
+                else if(password.isEmpty() ){
+                    passwordEditText.setError("Please enter password");
+                    passwordEditText.requestFocus();
+                }
+
+                else if(confirmPassword.isEmpty() ){
+                    confirmPasswordEditText.setError("Please enter password");
+                    confirmPasswordEditText.requestFocus();
+                }
+
+                else if(!password.equals(confirmPassword) ){
+                    Toast.makeText(SignUp.this,"Passwords dont match",Toast.LENGTH_SHORT).show();
+                }
+
+
+                else if (!(email.isEmpty() && password.isEmpty() && confirmPassword.isEmpty())){
+                    mFirebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(SignUp.this,
+                            new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if(!task.isSuccessful()){
+                                        Toast.makeText(SignUp.this,"Signup Unsuccessful, try again",Toast.LENGTH_SHORT).show();
+                                    }
+                                    else {
+                                        startActivity(new Intent(SignUp.this, MainActivity.class));  //change this class once homescreen is created
+                                    }
+                                }
+                            });
+                }
+
+            }
+        });
+
+
+        signInTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SignUp.this,MainActivity.class));
+            }
+        });
+
+    }
+}
