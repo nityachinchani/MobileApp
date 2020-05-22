7d2b27ac67e2391a0b358e3ab57ff3fb8410e333
nityachinchani <nitya.chinchani@gmail.com>
2020-05-21 21:16:43 -0700

    Introduce Utility

---
+++ app/src/main/java/com/example/coen268project/Presentation/Utility.java
@@ -0,0 +1,25 @@
+package com.example.coen268project.Presentation;
+
+public class Utility {
+
+    public enum ItemStatus
+    {
+        POSTED,
+        SOLD,
+        BOOKED
+    }
+
+    public enum CommunicationType
+    {
+        TEXT,
+        AUDIO,
+        VIDEO
+    }
+
+    public enum BillingStatus
+    {
+        PENDING,
+        DELIVERED,
+        RECEIVED
+    }
+}
