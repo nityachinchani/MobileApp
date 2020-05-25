2fc5872be280b45ab9d1a9c8a2d4e215a39b8f1d
Sarvesh Kulkarni <30865213+snk95@users.noreply.github.com>
2020-05-25 03:18:50 -0700

    Fixed the container view in fragment

--- app/src/main/java/com/example/coen268project/View/Account_fragment.java
+++ app/src/main/java/com/example/coen268project/View/Account_fragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,9 +13,11 @@ import androidx.fragment.app.Fragment;
 import com.example.coen268project.R;
 
 public class Account_fragment extends Fragment {
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.account_fragment,container,false);
     }
 }
