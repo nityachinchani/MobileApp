53e5b480f20c8428b8c60b55b4668ede47c42b5f
Sarvesh Kulkarni <30865213+snk95@users.noreply.github.com>
2020-05-29 13:36:04 -0700

    Explore buy screen

--- app/src/main/java/com/example/coen268project/View/Explore.java
+++ app/src/main/java/com/example/coen268project/View/Explore.java
@@ -1,5 +1,6 @@
 package com.example.coen268project.View;
-import com.example.coen268project.Presentation.Utility;
+import com.example.coen268project.Model.ItemDao;
+import com.example.coen268project.Presentation.Item;
 import com.example.coen268project.R;
 
 import android.content.Intent;
@@ -9,19 +10,25 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.GridLayout;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import java.util.Locale;
+import java.util.ArrayList;
 
 public class Explore extends Fragment {
     private static final String[] titles = new String[]{"Item1", "Item2",
-            "Item3", "Item4", "Item5"};
+            "Item3", "Item4", "Item5","Item6", "Item7", "Item8","Item9", "Item10"};
+
+    int[] numberImage={R.drawable.test1,R.drawable.test2,R.drawable.test3,R.drawable.test4,R.drawable.test5,R.drawable.test6
+    ,R.drawable.ic_account_box_black_24dp,R.drawable.ic_camera_alt_black_24dp,R.drawable.ic_chat_bubble_black_24dp,R.drawable.ic_current
+    ,R.drawable.ic_find_in_page_black_24dp};
 
     private static final String[] category = new String[]{"Sofa and Dining", "Bed and Wardrobes",
             "Home Decor and Garden", "Kids Furniture", "Other Household Items"};
@@ -31,32 +38,52 @@ public class Explore extends Fragment {
 
     private ListView exploreListView;
     private Spinner locationSpinner, categorySpinner;
+    private GridView exploreGridView;
+    private TextView textView;
+    private Item item;
+    private ArrayList<ItemDao> itemList = new ArrayList<>();
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.explore_fragment, container, false);
 
-        exploreListView = (ListView) view.findViewById(R.id.explore_List_View);
-        categorySpinner = (Spinner)view.findViewById(R.id.categorySpinner);
-        locationSpinner = (Spinner)view.findViewById(R.id.locationSpinner);
+        //exploreListView = (ListView) view.findViewById(R.id.explore_List_View);
+        categorySpinner = (Spinner) view.findViewById(R.id.categorySpinner);
+        locationSpinner = (Spinner) view.findViewById(R.id.locationSpinner);
+        exploreGridView=(GridView)view.findViewById(R.id.exploreGridView);
 
-
-
-        ArrayAdapter<String> adapter=new ArrayAdapter<String>(getActivity(), R.layout.activity_row,R.id.text_id,titles);
-        exploreListView.setAdapter(adapter);
-
-        exploreListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), R.layout.activity_row, R.id.text_id, titles);
+//        exploreListView.setAdapter(adapter);
+//        item = new Item();
+//        item.getAllItems(new CallBack() {
+//            @Override
+//            public void onSuccess(Object object) {
+//                Object[] objectArray = (Object[]) object;
+//                for (Object itemElement : objectArray
+//                ) {
+//                    itemList.add((ItemDao) itemElement);
+//                }
+//                BindItems();
+//            }
+//
+//            @Override
+//            public void onError(Object object) {
+//            }
+//        });
+        MainAdapter_explore_screen adapter= new MainAdapter_explore_screen(getActivity(),titles,numberImage);
+        exploreGridView.setAdapter(adapter);
+        exploreGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
-                Intent intent = new Intent(getActivity(), location_fragment.class);
-                intent.putExtra("Item", (CharSequence) exploreListView.getItemAtPosition(i+1));
+               // Toast.makeText(getContext(),"You clicked "+titles[+i],Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getActivity(), Explore_buy.class);
                 startActivity(intent);
+
             }
         });
 
-        ArrayAdapter<String> categoryAdapter=new ArrayAdapter<String>(getActivity(), R.layout.activity_spinner_row,R.id.text_id,category);
+        ArrayAdapter<String> categoryAdapter = new ArrayAdapter<String>(getActivity(), R.layout.activity_spinner_row, R.id.text_id, category);
         categorySpinner.setAdapter(categoryAdapter);
         categorySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -73,7 +100,7 @@ public class Explore extends Fragment {
         });
 
 
-        ArrayAdapter<String> locationAdapter=new ArrayAdapter<String>(getActivity(), R.layout.activity_spinner_row,R.id.text_id,location);
+        ArrayAdapter<String> locationAdapter = new ArrayAdapter<String>(getActivity(), R.layout.activity_spinner_row, R.id.text_id, location);
         locationSpinner.setAdapter(locationAdapter);
         locationSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -94,9 +121,53 @@ public class Explore extends Fragment {
     }
 
 
-    @Override
-    public void onCreate(Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
+    public void BindItems() {
+        final CustomAdapter adapter = new CustomAdapter(itemList);
+//        exploreListView.setAdapter(adapter);
+//        exploreListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                Intent intent = new Intent(getActivity(), location_fragment.class);
+//                intent.putExtra("Item", (CharSequence) adapter.getItem(i));
+//                startActivity(intent);
+//            }
+//        });
+    }
 
+    public class CustomAdapter extends BaseAdapter {
+        ArrayList<ItemDao> items = new ArrayList<>();
+
+        public CustomAdapter(ArrayList<ItemDao> itemList) {
+            for (ItemDao item : itemList
+            ) {
+                this.items.add(item);
+            }
+        }
+
+
+        @Override
+        public int getCount() {
+            return items.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return this.items.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = getLayoutInflater();
+            View activity_row;
+            activity_row = inflater.inflate(R.layout.activity_row, parent, false);
+            textView = activity_row.findViewById(R.id.text_id);
+            textView.setText(this.items.get(position).getItemName());
+            return activity_row;
+        }
     }
 }
---
+++ app/src/main/java/com/example/coen268project/View/Explore_buy.java
@@ -0,0 +1,14 @@
+package com.example.coen268project.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.coen268project.R;
+import android.os.Bundle;
+
+public class Explore_buy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_explore_buy);
+    }
+}
