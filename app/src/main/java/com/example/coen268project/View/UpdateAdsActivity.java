abcf47ee6210c02228e7ad9eaec5b4134e52a80d
nityachinchani <nitya.chinchani@gmail.com>
2020-06-07 13:58:44 -0700

    re-merge rollback changes

--- app/src/main/java/com/example/coen268project/View/UpdateAdsActivity.java
+++ app/src/main/java/com/example/coen268project/View/UpdateAdsActivity.java
@@ -106,7 +106,9 @@ public class UpdateAdsActivity extends AppCompatActivity {
                 updates.put("description", product_description);
                 updates.put("price", product_price);
                 updates.put("itemStatus", statusSpinner.getSelectedItem().toString());
-                updates.put("pictureName",product_picture_name[0]);
+                if(!product_picture_name.toString().isEmpty()) {
+                    updates.put("pictureName", product_picture_name[0]);
+                }
                 item.updateItem(itemId, (HashMap) updates, new CallBack() {
                     @Override
                     public void onSuccess(Object object) {
